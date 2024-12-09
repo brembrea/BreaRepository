13d748574435dcd96479f3b770c6bb97d36cc852
brembrea <812389@seq.org>
2024-12-09 12:51:35 -0800

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".blurred").on("click", function () {
-      $(this).css("filter", "none");
-      $(this).siblings(".tw-label").fadeOut();
-    });
-  });
-  
